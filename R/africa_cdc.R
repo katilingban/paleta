6f4922d29cbbc3386b136b1f6c27cedcad8d7705
Ernest Guevarra <ernest@guevarra.io>
2024-03-02 10:55:33 +0000

    initiate ACDC palette

---
+++ R/africa_cdc.R
@@ -0,0 +1,13 @@
+#'
+#' Africa CDC colours
+#'
+#' @examples
+#' acdc_palette
+#'
+#'
+#' @export
+#'
+acdc_palette <- list(
+  "acdc_primary" = c("#348F41", "#B4A269", "#000000"),
+  "acdc_secondary" = c("#6B4C24", "#E08F2A", "#C45B39", "#782C2D", "#4B5430")
+)
---
+++ R/data.R
@@ -0,0 +1,18 @@
+#'
+#' Colours based on visual identity guidelines of various organisations
+#'
+#' @format A data frame with 5 fields
+#'
+#' | **Variable** | **Description** |
+#' | :--- | :--- |
+#' | *name* | Organisation colour name |
+#' | *rgb* | Three integers for the red, green, blue components of the RGB colour model |
+#' | *cmyk* | Four integers for the cyan, magenta, yellow, and black components of the CMYK colour model |
+#' | *hex* | Hexadecimal codes for corresponding colour |
+#' | *pantone* | Pantone colour name |
+#'
+#' @examples
+#' motif_colours
+#'
+#'
+"motif_colours"
